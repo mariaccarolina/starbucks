2710c93195d6bf3e237ccfb43c10c7fd74e48f05
Maria Carolina <demycar24@gmail.com>
2024-11-25 14:57:56 -0300

    atualizaçoes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,71 @@
+import { useState } from "react";
+import {MainStyle, DivFuncional, Copo} from "./style"
 export default function App() {
-  return(
-    <>
-    <h1>Starbucks</h1>
-    </>
-  )
+  //estado para controlar o café selecionado:
+  const [copoSelecionado, setCompoSelecionado] = useState(
+    "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/laranja2x.png?raw=true"
+  );
+  const [corDeFundo, setCorDeFundo] = useState(
+    "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/Ellipse%202.png?raw=true"
+  );
+
+  const trocandoCopo = (imagemCopo,imagemFundo) => {
+    setCompoSelecionado(imagemCopo);
+    setCorDeFundo(imagemFundo);
+  }
+  //
+  return (
+    <MainStyle>
+      <section>
+        <h2>Mais que Café</h2>
+        <h2>
+          Isso é <span>Starbucks</span>
+        </h2>
+        <p>
+          A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns
+          dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o
+          Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas
+          quentes e frias, doces diferenciados e sanduíches.
+        </p>
+        <button>SAIBA MAIS</button>
+        <div>
+          <img
+            src="https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/amarelo.png?raw=true"
+            alt=""
+            onClick={() =>
+              trocandoCopo(
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/amarelo2x.png?raw=true",
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/Ellipse%201.png?raw=true"
+              )
+            }
+          />
+          <img
+            src="https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/laranja.png?raw=true"
+            alt=""
+            onClick={() =>
+              trocandoCopo(
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/laranja2x.png?raw=true",
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/Ellipse%202.png?raw=true"
+              )
+            }
+          />
+          <img
+            src="https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/vermelho.png?raw=true"
+            alt=""
+            onClick={() =>
+              trocandoCopo(
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/vermelho2x.png?raw=true",
+                "https://github.com/mariaccarolina/desafioSurpresa/blob/main/src/assets/images/vermelhoElipse.png?raw=true"
+              )
+            }
+          />
+        </div>
+      </section>
+
+      <DivFuncional>
+        <Copo src={copoSelecionado} alt="" />
+        <img src={corDeFundo} alt="" />
+      </DivFuncional>
+    </MainStyle>
+  );
 }
---
+++ src/style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const MainStyle = styled.main`
+display: flex;
+align-items: center;
+height:80vh;
+`
+
+export const DivFuncional = styled.div`
+position: relative;
+
+`
+
+export const Copo = styled.img`
+position: relative;
+top: 400px;
+left: 100px;
+`
